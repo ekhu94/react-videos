ec60d8bc8d283fdded53a33435c995ff1c545384
Erik Huang <erikhuangdrums@gmail.com>
2021-03-13 19:19:51 -0800

    axios create with youtube api URL and params

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAFL9HZSzsPUp3sCPdwolLEpTXwUn9J2_w';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+        type: 'video'
+    }
+});
