edf2b36a12bf409442da6a8707f8a1f7876d4e01
Erik Huang <erikhuangdrums@gmail.com>
2021-03-14 07:22:47 -0700

    create VideoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
@@ -16,13 +17,14 @@ class App extends React.Component {
     }
 
     onVideoSelect = video => {
-        console.log(video);
+        this.setState({ selectedVideo: video });
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSearchSubmit={ this.onSearchSubmit } />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList videos={ this.state.videos } onVideoSelect={ this.onVideoSelect } />
             </div>
         );
